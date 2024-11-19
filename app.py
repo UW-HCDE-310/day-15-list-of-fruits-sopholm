c486fa7cc22468bb18f37fd10fc73ee0a21b94e3
sophie <sopholm@uw.edu>
2024-11-19 10:42:02 -0800

    code added

--- app.py
+++ app.py
@@ -8,4 +8,8 @@ def index():
             {"name": "oranges", "quantity": 2},
             {"name": "strawberries", "quantity": 6}
     ]
+    #return render_template("index.html", fruits=fruits)
+
+#fruits that have more than 3 of
+    fruits = [fruit for fruit in fruits if fruit["quantity"] > 3]
     return render_template("index.html", fruits=fruits)
