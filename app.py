6a86098c3e8e9890cb7f8e8d409e2830460d2372
sophie <sopholm@uw.edu>
2024-11-19 22:16:42 -0800

    ignore keys

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
             {"name": "oranges", "quantity": 2},
             {"name": "strawberries", "quantity": 6}
     ]
-    return render_template("index.html", fruits=fruits, key_1=MY_SECRET_API_KEY_1, key_2=MY_SECRET_API_KEY_2 )
+    return render_template("index.html", fruits=fruits, key_1 = MY_SECRET_API_KEY_1, key_2 = MY_SECRET_API_KEY_2 )
 
 #fruits that have more than 3 of
     #fruits = [fruit for fruit in fruits if fruit["quantity"] > 3]
